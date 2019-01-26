a07443f9d3cf0ffecd88b423efb8864a6f6d7eb8
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2019-01-26 12:28:28 -0600

    Select BowTie, then Editor, after that: Create NSManagedObject subclass

---
+++ BowTie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  BowTie+CoreDataClass.swift
+//  Bow Ties
+//
+//  Created by Carlos Santiago Cruz on 1/26/19.
+//  Copyright © 2019 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(BowTie)
+public class BowTie: NSManagedObject {
+
+}
---
+++ BowTie+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  BowTie+CoreDataProperties.swift
+//  Bow Ties
+//
+//  Created by Carlos Santiago Cruz on 1/26/19.
+//  Copyright © 2019 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension BowTie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<BowTie> {
+        return NSFetchRequest<BowTie>(entityName: "BowTie")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var isFavorite: Bool
+    @NSManaged public var lastWorn: NSDate?
+    @NSManaged public var rating: Double
+    @NSManaged public var searchKey: String?
+    @NSManaged public var timesWorn: Int32
+    @NSManaged public var id: UUID?
+    @NSManaged public var url: URL?
+    @NSManaged public var photoData: NSObject?
+
+}
